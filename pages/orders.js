5a5d55dd14604bccbe703e0c0d3ded888c19aa80
Shawn Kebel <fxrender@outlook.com>
2023-05-13 16:36:53 -0500

    :construction: Start `Orders` page

--- pages/orders.js
+++ pages/orders.js
@@ -1,9 +1,46 @@
 import Layout from "@/components/Layout";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-export default function Orders() {
+export default function OrdersPage() {
+  const [orders, setOrders] = useState([]);
+  useEffect(() => {
+    axios.get('/api/orders').then(response => {
+      setOrders(response.data);
+    });
+  }, []);
+
   return (
     <Layout>
       <h1>Orders</h1>
+      <table className="basic">
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Recipient</th>
+            <th>Products</th>
+          </tr>
+        </thead>
+        <tbody>
+          {orders.length > 0 && orders.map(order => (
+            <tr key={order._id}>
+              <td>{order.createdAt}</td>
+              <td>{order.name} {order.email}<br />
+                {order.streetAddress}<br />
+                {order.city} {order.state}<br />
+                {order.country} {order.postalCode}
+              </td>
+              <td>
+                {order.line_items.map(l => (
+                  <>
+                  {JSON.stringify(l)}<br />
+                  </>
+                ))}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </Layout>
   )
 }
